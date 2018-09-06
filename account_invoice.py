8fe4ef1e52c224d984c400f95611f0fc076507c5
Tony Galmiche <tony.galmiche@infosaone.com>
2018-09-06 16:26:36 +0200

    modif facture pour bressane

--- account_invoice.py
+++ account_invoice.py
@@ -4,12 +4,19 @@ from openerp import models,fields,api
 from openerp.tools.translate import _
 
 
+
+class is_mode_reglement(models.Model):
+    _name = "is.mode.reglement"
+    name = fields.Char(string='Mode de règlement')
+
+
 class account_invoice(models.Model):
     _inherit = "account.invoice"
 
     is_acompte               = fields.Float("Acompte")
     is_imputation_partenaire = fields.Char("Imputation partenaire")
     is_contact_id            = fields.Many2one('res.partner', string='Contact')
+    is_mode_reglement_id     = fields.Many2one('is.mode.reglement', string='Mode de règlement')
 
 
 class account_invoice_line(models.Model):
