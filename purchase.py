6171b93f4b46b7b56ddf1933ffc403773e0671f5
Tony Galmiche <tony.galmiche@infosaone.com>
2018-11-22 11:07:08 +0100

    add buton lignes de réceptions sur commandes

--- purchase.py
+++ purchase.py
@@ -9,3 +9,30 @@ class purchase_order_line(models.Model):
 
     is_date_ar = fields.Date("Date AR")
 
+
+
+class purchase_order(models.Model):
+    _inherit = "purchase.order"
+
+
+    @api.multi
+    def mouvement_stock_action(self):
+        for obj in self:
+            ids=[]
+            for picking in obj.picking_ids:
+                for line in picking.move_lines:
+                    ids.append(line.id)
+            if not ids:
+                raise Warning(u'Aucune ligne de réception')
+            else:
+                return {
+                    'name': u'Lignes de réception '+obj.name,
+                    'view_mode': 'tree,form',
+                    'view_type': 'form',
+                    'res_model': 'stock.move',
+                    'domain': [
+                        ('id','in',ids),
+                    ],
+                    'type': 'ir.actions.act_window',
+                }
+
